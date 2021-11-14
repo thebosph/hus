9eb032126a2f3a2fc7e335369d961fec153824e5
thebosph <haciibrahimsimsek@ogr.iu.edu.tr>
2021-11-14 16:29:24 +0300

    sanity env var.
    usually sanity client doesnt read env.

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "383bfbp1", // you can find this in sanity.json
-  dataset: "production", // or the name you chose in step 1
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: "2021-08-31",
 });
