5f8625340c25e15f7d1c89c5a3a29c35a27233f5
thebosph <haciibrahimsimsek@ogr.iu.edu.tr>
2021-11-14 12:36:06 +0300

    sorry

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID, // you can find this in sanity.json
-  dataset: process.env.SANITY_DATASET, // or the name you chose in step 1
+  projectId: "383bfbp1", // you can find this in sanity.json
+  dataset: "production", // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: "2021-08-31",
 });
